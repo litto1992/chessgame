a7b7a247780522c9bea134d37dce2d682656ceb9
litto1992 <littojohn.thankachan4@gmail.com>
2025-08-14 18:01:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+alert("Refresh Me!!!");
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
 if (randomNumber1 === 1) {
    document.querySelector(".img1").setAttribute("src", "images/dice1.png");
